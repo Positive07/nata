4b6fc9f5f15047d2e1df9eacf397528e0d547548
Andrew Minnich <aminnich3@gmail.com>
2017-11-06 03:04:46 -0500

    make the indentation less yikes

--- nata.lua
+++ nata.lua
@@ -56,70 +56,70 @@ function Pool:add(entity, ...)
 		end
 		table.insert(self._entities, entity)
 	end
-    return entity
+	return entity
 end
 
 function Pool:remove(f, ...)
-    f = f or function() return true end
-    for i = #self._entities, 1, -1 do
-        local entity = self._entities[i]
-        if f(entity) then
-            for j = 1, #self._systems do
+	f = f or function() return true end
+	for i = #self._entities, 1, -1 do
+		local entity = self._entities[i]
+		if f(entity) then
+			for j = 1, #self._systems do
 				local system = self._systems[j]
-                local filter = system.filter or function()
-                    return true
-                end
-                if system.remove and filter(entity) then
-                    system.remove(entity, ...)
+				local filter = system.filter or function()
+					return true
+				end
+				if system.remove and filter(entity) then
+					system.remove(entity, ...)
 				end
 				for k = #self._cache[system], 1, -1 do
 					if self._cache[system][k] == entity then
 						table.remove(self._cache[system], k)
 					end
 				end
-            end
-            table.remove(self._entities, i)
-        end
-    end
+			end
+			table.remove(self._entities, i)
+		end
+	end
 end
 
 function Pool:get(f)
-    if f then
-        local entities = {}
-        for i = 1, #self._entities do
-            local entity = #self._entities[i]
-            if f(entity) then
-                table.insert(entities, entity)
-            end
-        end
-        return entities
-    else
-        return self._entities
-    end
+	if f then
+		local entities = {}
+		for i = 1, #self._entities do
+			local entity = #self._entities[i]
+			if f(entity) then
+				table.insert(entities, entity)
+			end
+		end
+		return entities
+	else
+		return self._entities
+	end
 end
 
 function Pool:callOn(entity, event, ...)
-    for i = 1, #self._systems do
-        local system = self._systems[i]
-        local filter = system.filter or function()
-            return true
-        end
-        if system[event] and filter(entity) then
-            system[event](entity, ...)
-        end
-    end
+	for i = 1, #self._systems do
+		local system = self._systems[i]
+		local filter = system.filter or function()
+			return true
+		end
+		if system[event] and filter(entity) then
+			system[event](entity, ...)
+		end
+	end
 end
 
 function Pool:call(event, ...)
 	self._calling = true
-    for i = 1, #self._systems do
-        local system = self._systems[i]
-        if system[event] then
-            for j = 1, #self._cache[system] do
-                local entity = self._cache[system][j]
+	for i = 1, #self._systems do
+		local system = self._systems[i]
+		if system[event] then
+			for j = 1, #self._cache[system] do
+				local entity = self._cache[system][j]
 				system[event](entity, ...)
-            end
-        end
+			end
+		end
 	end
 	self._calling = false
 	self:_flushQueue()
@@ -148,13 +148,13 @@ nata.oop = setmetatable({}, {
 })
 
 function nata.new(systems)
-    return setmetatable({
-        _systems = systems or {nata.oop},
+	return setmetatable({
+		_systems = systems or {nata.oop},
 		_entities = {},
 		_cache = {},
 		_queue = {},
 		_calling = false,
-    }, {__index = Pool})
+	}, {__index = Pool})
 end
 
 return nata
